5481da2477506c9f98e0adf84a802e535752a2a8
bartek maciejewski <bartlomiej.maciejewski@adtoma.com>
2017-05-29 16:55:26 +0200

    Adding new class to check

---
+++ Classes/ExampleHeader.h
@@ -0,0 +1,15 @@
+//
+//  ExampleHeader.h
+//  Pods
+//
+//  Created by BARTEK on 29/05/2017.
+//
+//
+
+#import "ExampleHeader.h"
+@interface ExampleH : NSObject
+
+- (BOOL)returnsTrue2;
+- (BOOL)returnsFalse2;
+
+@end
---
+++ Classes/ExampleHeader.m
@@ -0,0 +1,22 @@
+//
+//  ExampleHeader.m
+//  Pods
+//
+//  Created by BARTEK on 29/05/2017.
+//
+//
+
+#import "ExampleHeader.h"
+
+@implementation ExampleH
+
+- (BOOL)returnsTrue2 {
+    return YES;
+}
+
+- (BOOL)returnsFalse2 {
+    return NO;
+}
+
+@end
+
